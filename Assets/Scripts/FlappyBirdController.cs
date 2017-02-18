5af6e1de0d3423a79c4ad5977e96d8941ff54b60
Adriano Gil <adrianogil.san@gmail.com>
2017-02-18 11:39:41 -0400

    FlappyBird animations

---
+++ Assets/Scripts/FlappyBirdController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlappyBirdController : MonoBehaviour {
+
+	[Header("Velocity")]
+	public float flappyVelocity;
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		// Convertendo de coordenadas de Tela para Mundo
+		Vector3 startPos = 
+			Camera.main.ViewportToWorldPoint (new Vector3 (0.2f, 0.8f));
+
+		startPos.z = -1.58f; // Para garantir visibilidade do FlappyBird
+
+		transform.position = startPos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
