907e2ae385ca9c9f84f516f1abba73d5963fab85
Adriano Gil <adrianogil.san@gmail.com>
2017-03-08 10:33:39 -0400

    Refactoring Scrolling behavior

--- Assets/Scripts/PipesManager.cs
+++ Assets/Scripts/PipesManager.cs
@@ -2,79 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PipesManager : MonoBehaviour {
+public class PipesManager : ScrollingManager {
 
-	[Header("References")]
-	public PipeScroll pipePrefab;
+	public float minYVar = -6f;
+	public float maxYVar = 6f;
 
-	[Header("Pipes")]
-	public int maxPipes;
-	public float pipesOffset;
-
-	private Transform[] pipes;
-	private int firstPipeIndex;
-
-	private float worldOffset;
-
-	// Use this for initialization
-	void Start () {
-		pipes = new Transform[maxPipes];
-
-		Vector3 pipePosition;
-
-		worldOffset = (
-			Camera.main.ViewportToWorldPoint (
-				new Vector3(pipesOffset, 0f, 0f)
-			) - 
-			(Camera.main.ViewportToWorldPoint (
-				new Vector3(0f, 0f, 0f)
-			))
-		).x;
-
-		for (int i = 0; i < maxPipes; i++) {
-			// Encontra a posicao de cada pipe
-			pipePosition = Camera.main.ViewportToWorldPoint (
-				new Vector3(1f + pipesOffset * i, 
-					Random.Range(-0.2f, 0.25f))
-			);
-			pipePosition.z = 0f;
-
-			// Criar cada pipe
-			pipes [i] = Instantiate(pipePrefab.transform) as Transform;
-			pipes [i].parent = transform;
-			pipes [i].localPosition = pipePosition;
-		}
-
-		firstPipeIndex = 0;
+	public override void OnObjectPlacement(GameObject goOnLastPosition)
+	{
+		// Define uma posicao aleatoria no eixo y
+        Vector3 pipesPos = goOnLastPosition.transform.localPosition;
+        pipesPos.y = Random.Range(minYVar, maxYVar);
+        goOnLastPosition.transform.localPosition = pipesPos;
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
-		Vector3 screenPos = 
-			Camera.main.WorldToViewportPoint (
-				pipes[firstPipeIndex].position
-			);
+	protected override Transform GenerateObject(Vector3 localPosition)
+	{
+		localPosition.y = Random.Range(minYVar, maxYVar);
 
-		if (screenPos.x < -0.5f) {
-			// Index da posicao anterior
-			int lastPipeIndex = 
-				(pipes.Length + firstPipeIndex - 1) % pipes.Length;
-
-			// Posiciona atras do ultimo pipe
-			pipes [firstPipeIndex].localPosition = 
-				pipes [lastPipeIndex].localPosition +
-			new Vector3 (worldOffset, 0f, 0f);
-
-			// Define uma posicao aleatoria no eixo y
-			Vector3 pipesPos = pipes [firstPipeIndex].localPosition;
-			pipesPos.y = Camera.main.ViewportToWorldPoint (
-				new Vector3(0f, Random.Range(-0.2f, 0.25f), 0f)
-			).y;
-			pipes [firstPipeIndex].localPosition = pipesPos;
-
-			// Atualiza index do primeiro elemento
-			firstPipeIndex = (firstPipeIndex + 1) % pipes.Length;
-		}
+		return base.GenerateObject(localPosition);
 	}
+
 }
---
+++ Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour {
+
+    public Vector3 scrollVelocity;
+    public Vector3 size;
+
+    public virtual void Update()
+    {
+        transform.position += scrollVelocity * Time.deltaTime;
+    }
+
+    [ContextMenu("Update Size From Children")]
+    public void UpdateSizeFromChildren()
+    {
+        Bounds bounds = new Bounds (transform.position, Vector3.one);
+        Renderer[] renderers = GetComponentsInChildren<Renderer> ();
+        foreach (Renderer renderer in renderers)
+        {
+            bounds.Encapsulate (renderer.bounds);
+        }
+
+        size = bounds.size;
+    }
+}
